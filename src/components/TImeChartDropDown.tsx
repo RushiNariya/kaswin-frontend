aa3a27090196352a6de8db596330bd71b583bed4
rushinariya <rushi.nariya@atliq.com>
2023-09-26 11:43:12 +0530

    CHG: ui changes

--- src/components/TImeChartDropDown.tsx
+++ src/components/TImeChartDropDown.tsx
@@ -59,7 +59,7 @@ export default function TimeChartDropDown() {
           }}
         >
           <div className="relative mt-1">
-            <Listbox.Button className="relative w-[170px] bg-[#F1F1F1] cursor-pointer rounded-lg bg-white py-2 pl-3 pr-10 text-left border focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+            <Listbox.Button className="relative w-[170px] bg-[#F1F1F1] cursor-pointer rounded-lg py-2 pl-3 pr-10 text-left border focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
               <span className="block truncate">{selected.value}</span>
               <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
                 <ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
@@ -71,7 +71,7 @@ export default function TimeChartDropDown() {
               leaveFrom="opacity-100"
               leaveTo="opacity-0"
             >
-              <Listbox.Options className="absolute bg-[#F1F1F1] z-50 border-1 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-xl  ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+              <Listbox.Options className="absolute bg-[#F1F1F1] z-50 border-1 mt-1 max-h-60 w-full overflow-auto rounded-md py-1 text-base shadow-xl  ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
                 {list.map((person, personIdx) => (
                   <Listbox.Option
                     key={personIdx}
